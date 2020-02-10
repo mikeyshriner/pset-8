b89668c2d744f3c2d8e9ca921dc8b47d0ad944dc
mikeyshriner <shrinermichael@gmail.com>
2020-02-09 19:00:30 -0500

    squares???

--- js/app.js
+++ js/app.js
@@ -20,5 +20,6 @@ window.onload = init;
 function render() {
   board.forEach(function(mark, index) {
     console.log(mark, index);
+     squares[index].textContent = mark;    // writes an X or an O on board
   });
 }
