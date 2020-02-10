a9ab0544127e39b74efb0251c04da02b5b140830
mikeyshriner <shrinermichael@gmail.com>
2020-02-09 19:02:11 -0500

    im a clown oops

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let board;
-
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+///////////////////// FUNCTIONS /////////////////////////////////////
 function init() {
   board = [
     "", "", "",
@@ -12,14 +16,9 @@ function init() {
 
   render();   // we'll write this later
 }
-///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-const squares = Array.from(document.querySelectorAll("#board div"));
-///////////////////// EVENT LISTENERS ///////////////////////////////
-window.onload = init;
-///////////////////// FUNCTIONS /////////////////////////////////////
 function render() {
   board.forEach(function(mark, index) {
     console.log(mark, index);
-     squares[index].textContent = mark;    // writes an X or an O on board
+     squares[index].textContent = mark;    // writes an X or an O on boards
   });
 }
